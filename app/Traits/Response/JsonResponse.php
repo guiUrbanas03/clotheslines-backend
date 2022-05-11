cfb2bd1a49094a721581cb8f38c5ba39c55a660e
guiUrbanas03 <guilherme_urbanas@hotmail.com>
2022-05-11 14:01:01 -0300

    💻 Adding json response trait

---
+++ app/Traits/Response/JsonResponse.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Traits\Response;
+
+trait JsonResponse
+{
+    public function jsonResponse($data, $code = 200)
+    {
+        return response()->json(
+            $data,
+            $code,
+            ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
+            JSON_UNESCAPED_UNICODE
+        );
+    }
+}
