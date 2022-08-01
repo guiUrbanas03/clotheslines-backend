1996968dcce24b3aef67b80106941a36a8d908f4
guiUrbanas03 <guilherme_urbanas@hotmail.com>
2022-08-01 03:41:59 +0100

    💻 Creating Playlist model

--- app/Models/Playlist.php
+++ app/Models/Playlist.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Http\Resources\Playlist\PlaylistResource;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -33,5 +34,10 @@ class Playlist extends Model
     public function comments()
     {
         return $this->morphMany(Comment::class, 'commentable');
+    }
+
+    public function getResourceAttribute()
+    {
+        return new PlaylistResource($this);
     }
 }
