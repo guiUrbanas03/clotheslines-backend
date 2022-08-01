2202c382f19590a46e94ea1e73cef44474d51933
guiUrbanas03 <guilherme_urbanas@hotmail.com>
2022-08-01 03:42:25 +0100

    💻 Creating PlaylistSeeder seeder

--- database/seeders/PlaylistSeeder.php
+++ database/seeders/PlaylistSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Playlist;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,6 @@ class PlaylistSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Playlist::factory(10)->create();
     }
 }
