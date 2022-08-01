1d025e7a0cd85f1314b90916a575143744995f1b
guiUrbanas03 <guilherme_urbanas@hotmail.com>
2022-08-01 03:44:03 +0100

    💻 Creating SongSeeder seeder

--- database/seeders/SongSeeder.php
+++ database/seeders/SongSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Song;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,6 @@ class SongSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Song::factory(50)->create();
     }
 }
