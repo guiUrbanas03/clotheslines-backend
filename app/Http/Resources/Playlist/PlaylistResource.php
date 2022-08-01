74cb814e13db09d0fd90a19657e5349b29c5fc14
guiUrbanas03 <guilherme_urbanas@hotmail.com>
2022-08-01 03:39:59 +0100

    💻 Creating Playlist resource

---
+++ app/Http/Resources/Playlist/PlaylistResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources\Playlist;
+
+use App\Http\Resources\Pagination\PaginationResource;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PlaylistResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'profile_id' => $this->profile_id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'songs' => $this->songs,
+        ];
+    }
+}
