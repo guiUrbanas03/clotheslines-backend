8eca742cdd3e71bbcf033900bf9d4479e985e693
guiUrbanas03 <guilherme_urbanas@hotmail.com>
2022-08-31 23:39:52 +0100

    💻 Updating Comment model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,12 +2,18 @@
 
 namespace App\Models;
 
+use App\Http\Resources\Comment\CommentResource;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'profile_id',
+        'text'
+    ];
 
     public function commentable()
     {
@@ -17,5 +23,10 @@ class Comment extends Model
     public function profile()
     {
         return $this->belongsTo(Profile::class);
+    }
+
+    public function getResourceAttribute()
+    {
+        return new CommentResource($this);
     }
 }
