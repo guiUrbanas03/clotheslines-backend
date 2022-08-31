0b68ca44b113a585d173a4803a6d12db7431cea1
guiUrbanas03 <guilherme_urbanas@hotmail.com>
2022-08-31 23:39:35 +0100

    💻 Creating Comment resource

---
+++ app/Http/Resources/Comment/CommentResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\Comment;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CommentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'text' => $this->text,
+            'profile' => $this->profile,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
+        ];
+    }
+}
